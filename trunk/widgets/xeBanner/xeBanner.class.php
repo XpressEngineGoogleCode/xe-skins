4a96117a27020a19afa248b6cd732014e451af21
emerald.zhangj@gmail.com <emerald.zhangj@gmail.com@c6e7c58e-d3c7-6c84-515c-93f982c0779f>
2011-08-29 10:52:29 +0000

    xeBanner modyfy

--- trunk/widgets/xeBanner/xeBanner.class.php
+++ trunk/widgets/xeBanner/xeBanner.class.php
@@ -15,52 +15,53 @@
          * 결과를 만든후 print가 아니라 return 해주어야 한다
          **/
         function proc($args) {
-            // 위젯 변수 설정
-            $widget_info->title_1 = $args->banner_title_1;
-            $widget_info->title_2 = $args->banner_title_2;
-            $widget_info->title_3 = $args->banner_title_3;
-            $widget_info->description_1 = $args->banner_description_1;
-            $widget_info->description_2 = $args->banner_description_2;
-            $widget_info->description_3 = $args->banner_description_3;
-            $widget_info->url_1 = $args->banner_url_1;
-            if(!$widget_info->url_1) $widget_info->url_1 = '#';
-            else if(!preg_match('/^http/i',$widget_info->url_1)) $widget_info->url_1 = 'http://'.$widget_info->url_1;
-            $widget_info->url_2 = $args->banner_url_2;
-            if(!$widget_info->url_2) $widget_info->url_2 = '#';
-            else if(!preg_match('/^http/i',$widget_info->url_2)) $widget_info->url_2 = 'http://'.$widget_info->url_2;
-            $widget_info->url_3 = $args->banner_url_3;
-            if(!$widget_info->url_3) $widget_info->url_3 = '#';
-            else if(!preg_match('/^http/i',$widget_info->url_3)) $widget_info->url_3 = 'http://'.$widget_info->url_3;
+            //number of the image
+            $imageNo = 5;
 
-            $banner_ko = $args->banner_ko;
-            $default_banner = $banner_ko;
-            $banner_en = $args->banner_en;
-            if(!$default_banner && $banner_en) $default_banner = $banner_en;
-            $banner_jp = $args->banner_jp;
-            if(!$default_banner && $banner_jp) $default_banner = $banner_jp;
-            $banner_zh = $args->banner_zh;
-            if(!$default_banner && $banner_zh) $default_banner = $banner_zh;
-            switch(Context::getLangType()) {
-                case 'jp' :
-                        $banner = $banner_jp;
-                    break;
-                case 'zh-TW' :
-                case 'zh-CN' :
-                        $banner = $banner_zh;
-                    break;
-                case 'en' :
-                        $banner = $banner_en;
-                    break;
-                default : 
-                        $banner = $banner_ko;
-                    break;
+            // 위젯 변수 설정
+            for($i=1;$i<=$imageNo;$i++){
+            	//title
+            	$bKey = 'banner_title_'.$i;
+            	$widget_info->info[$i]['title'] = $args->$bKey;
+
+            	//descirption
+            	$bKey = 'banner_description_'.$i;
+            	$widget_info->info[$i]['descirption'] = $args->$bKey;
+
+            	//url
+            	$bKey = 'banner_url_'.$i;
+            	$widget_info->info[$i]['url'] = $args->$bKey;
+            	if(!$widget_info->info[$i]['url']) {
+            		$widget_info->info[$i]['url'] = '#';
+            	}
+            	else if(!preg_match('/^http/i',$widget_info->info[$i]['url'])){
+            		$widget_info->info[$i]['url'] = 'http://'.$widget_info->info[$i]['url'];
+            	}
+
+            	//image
+            	$key = 'banner_'.$i;
+            	if(empty($args->$key)){
+            		$widget_info->info[$i]['image'] = '';
+            		continue;
+            	}
+            	$widget_info->info[$i]['image'] = $args->$key;
             }
-            if(!$banner) $banner = $default_banner;
-            $widget_info->banner_image = $banner;
+
             Context::set('widget_info', $widget_info);
 
-            // 배너 이미지를 header에 추가
-            if($widget_info->banner_image) Context::addHtmlHeader('<style type="text/css"> .xeBanner .section { background:url('.$widget_info->banner_image.') no-repeat;} </style>');
+//            // 배너 이미지를 header에 추가
+//        	$style = '<style type="text/css">';
+//            foreach($widget_info->info as $key => $row){
+//            	if(empty($row['image'])){
+//            		continue;
+//            	}
+//            	$style .= '
+//            				.xeBanner .section'.@$key.' { background:url('.@$row['image'].') no-repeat;}';
+//            }
+//            $style .= '
+//            			</style>';
+//            Context::addHtmlHeader($style);
+
 
             // 1~3중 순서대로 처리
             if(!$_COOKIE['xb']) $_COOKIE['xb']=0;
@@ -80,4 +81,3 @@
             return $oTemplate->compile($tpl_path, $tpl_file);
         }
     }
-?>
