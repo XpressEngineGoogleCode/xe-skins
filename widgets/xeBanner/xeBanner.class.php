27578fa274f18df5f34278b7ab03fa8462385678
emerald.zhangj <emerald.zhangj@gmail.com>
2011-09-01 02:00:22 +0000

    1.banner可以多个

--- widgets/xeBanner/xeBanner.class.php
+++ widgets/xeBanner/xeBanner.class.php
@@ -60,6 +60,7 @@
 
             //no image add default images
             if(!count($widget_info->info)){
+            	$widget_info->is_default = true;
             	for($i=1;$i<=$imageNo;$i++){
             		$key = 'banner_'.$i;
             		$widget_info->info[$i]['url'] = 'javascript:void()';
@@ -67,7 +68,12 @@
             	}
             }
 
+            //css name
+            $widget_info->classPre = rand();
+
             Context::set('widget_info', $widget_info);
+
+
 
             // 템플릿의 스킨 경로를 지정 (skin, colorset에 따른 값을 설정)
             $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
